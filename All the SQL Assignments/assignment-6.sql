b4e8756bedc5d5abeba4d545576d3449201c0bbe
jaberabed <42500317+jaberabed@users.noreply.github.com>
2023-07-17 16:16:09 -0400

    Create assignment-6.sql

---
+++ All the SQL Assignments/assignment-6.sql
@@ -0,0 +1,3 @@
+--Use an appropriate merge to merge that new data-frame from the last step with a dataframe from the dvd db, that gives the countries, so now countries are connected with their classification. (no need to add this table back to the postgresql db, just doing it in the notebook is fine)
+
+select * from country;
