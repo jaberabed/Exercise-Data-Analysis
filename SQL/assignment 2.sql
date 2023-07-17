8d4d6507469966cc2cc3b1025850216d1e70c497
jaberabed <42500317+jaberabed@users.noreply.github.com>
2023-07-17 16:16:56 -0400

    Delete SQL directory

--- SQL/assignment 2.sql
+++
@@ -1,2 +0,0 @@
---Create a visual in the notebook that shows how much revenue is coming from different countries to the DVD store
-select title,description from public.film
--- SQL/assignment 4.sql
+++
@@ -1,8 +0,0 @@
---Do an analysis to show which actors are bringing in the most revenue (top 10 ranked)
-		
-select concat(a.first_name,' ',a.last_name) as actor_name ,sum(p.amount) as revenue from payment p
-join rental r on r.rental_id=p.rental_id
-join inventory i on i.inventory_id=r.inventory_id
-join film_actor fa on fa.film_id=i.film_id
-join actor a on a.actor_id=fa.actor_id
- group by concat(a.first_name,' ',a.last_name);
